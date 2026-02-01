572ae88761dda4123276bc46a82fa77f7d263aee
Arjun Pandey <arjun7236201798@gmail.com>
2026-02-01 21:02:28 +0530

    userRoute completed and tested post method

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -2,30 +2,38 @@ import { User } from "../model/usermodel.js";
 export const register = async (req, res) => {
     try {
         const { email, firstName, lastName, password } = req.body
+        console.log("BODY:", req.body);
         if (!email || !password || !firstName || !lastName) {
-            res.status(400).json({
+            return res.status(400).json({
                 success: false,
                 message: "All Fields Are Required"
             })
-            const user = await User.findOne({ email })
+        }
+        const user = await User.findOne({ email })
+        if (user) {
             res.status(400).json({
                 success: false,
                 message: "User Already Exist."
             })
+        } else {
             const newUser = await User.create({
                 firstName,
                 lastName,
                 email,
                 password
             })
-            await newUser.save()
-            return res.status(201).json({
-                success:true,
-                message:"User Registered Successfully",
-                user:newUser
-            })
+             return res.status(201).json({
+            success: true,
+            message: "User Registered Successfully",
+            user: newUser
+        })
         }
-    }catch(error){
+
+
+
+
+
+    } catch (error) {
         res.status(500).json({
             success: false,
             message: "Register Controller " + error.message
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 import express from 'express'
 import connectDB from './database/db.js'
+import userRoute from './routes/userRoute.js'
 const app = express()
 
 import "dotenv/config"
 
-const port =process.env.PORT||1000
+const port = process.env.PORT || 1000
+app.use(express.json())
+app.use('/api/user', userRoute)
 app.get('/', (req, res) => {
   res.send('Hello World!')
-}) 
-          
-app.listen(port, () => { 
+})
+
+app.listen(port, () => {
   connectDB()
   console.log(` Listening on port ${port}`)
 }) 

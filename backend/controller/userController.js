938f7f977d4dc374bd113cab8ef610d7cbf62e0b
Arjun Pandey <arjun7236201798@gmail.com>
2026-02-01 20:09:19 +0530

    UserController Added

---
+++ backend/controller/userController.js
@@ -0,0 +1,34 @@
+import { User } from "../model/usermodel.js";
+export const register = async (req, res) => {
+    try {
+        const { email, firstName, lastName, password } = req.body
+        if (!email || !password || !firstName || !lastName) {
+            res.status(400).json({
+                success: false,
+                message: "All Fields Are Required"
+            })
+            const user = await User.findOne({ email })
+            res.status(400).json({
+                success: false,
+                message: "User Already Exist."
+            })
+            const newUser = await User.create({
+                firstName,
+                lastName,
+                email,
+                password
+            })
+            await newUser.save()
+            return res.status(201).json({
+                success:true,
+                message:"User Registered Successfully",
+                user:newUser
+            })
+        }
+    }catch(error){
+        res.status(500).json({
+            success: false,
+            message: "Register Controller " + error.message
+        })
+    }
+}

713945f95bc7892e1b73e261d8ed94a7b1dc6566
Arjun Pandey <arjun7236201798@gmail.com>
2026-02-05 23:58:17 +0530

    login in fronted connected axios and testing is succesful

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react"
+import React, { use, useState } from "react"
 import { Button } from "@/components/ui/button"
-import { Eye, EyeOff } from "lucide-react"
+import { Eye, EyeOff, Loader2 } from "lucide-react"
 
 import {
     Card,
@@ -12,19 +12,88 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { Link } from "react-router"
+import { Link, useNavigate } from "react-router"
+import axios from "axios"
+import { toast, ToastContainer } from "react-toastify"
 
 function Login() {
     const [showPassword, setShowPassword] = useState(false)
+      const [loading, setLoading] = useState(false)
+    const navigate=useNavigate()
+    const [formData, setFormdata] = useState({
+        email: "",
+        password: ""
+    })
+    const handleChange = (e) => {
+        // e.preventDefault()
+        const { name, value } = e.target
+        setFormdata((prev) => ({
+            ...prev, [name]: value
+        }))}
+        const handleLogin = async (e) => {
+           
+            e.preventDefault()
+             
+            console.log(formData)
+            try {
+                const res = await axios.post(`http://localhost:3000/api/user/login`, formData, {
+                    headers: {
+                        "Content-Type": "Application/json"
+                    }
+                })
+setLoading(true)
+                if (res.data.success) {
+                    toast.success("Logged in successfully 😄", {
+                        position: "top-center",
+                        autoClose: 3000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "colored",
+                    });
+                }
+                setTimeout(() => {
+                    navigate('/home')
+                }, 3000);
+            } catch (error) {
+                const backendMessage =
+                    error.response?.data?.message || "Something went wrong ❌";
 
+                toast.error(backendMessage, {
+                    position: "top-center",
+                    autoClose: 4000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    theme: "colored",
+                });
+
+                console.log("Backend error 👉", error.response?.data);
+            }
+        }
+    
     return (
-    <div className="min-h-screen flex items-center justify-center 
+        <div className="min-h-screen flex items-center justify-center 
 bg-gradient-to-br from-white via-blue-50 to-pink-50
 
  ">
 
 
             <Card className="w-full max-w-sm rounded-2xl border border-border/60 shadow-2xl bg-card">
+<ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        draggable
+        theme="dark"  
+        limit={3}
+      />
 
                 {/* HEADER */}
                 <CardHeader className="space-y-3 pb-6">
@@ -58,6 +127,9 @@ bg-gradient-to-br from-white via-blue-50 to-pink-50
                             <Input
                                 id="email"
                                 type="email"
+                                name="email"
+                                value={formData.email}
+                                onChange={handleChange}
                                 placeholder="Enter your email"
                                 className="h-11 rounded-xl"
                                 required
@@ -71,7 +143,7 @@ bg-gradient-to-br from-white via-blue-50 to-pink-50
                                     Password
                                 </Label>
                                 <a
-                                    href="#"
+                                    href="/forgotpassword"
                                     className="text-xs font-medium text-primary hover:underline"
                                 >
                                     Forgot Password?
@@ -81,6 +153,10 @@ bg-gradient-to-br from-white via-blue-50 to-pink-50
                             <div className="relative">
                                 <Input
                                     id="password"
+                                    name="password"
+                                    placeholder="Enter Your Password"
+                                    value={formData.password}
+                                       onChange={handleChange}
                                     type={showPassword ? "text" : "password"}
                                     className="h-11 rounded-xl pr-10"
                                     required
@@ -106,9 +182,17 @@ bg-gradient-to-br from-white via-blue-50 to-pink-50
 
                 {/* FOOTER */}
                 <CardFooter className="flex flex-col gap-4 pt-6">
-                    <Button className="w-full h-11 rounded-xl">
-                        Login
-                    </Button>
+                      <Button
+         
+              disabled={loading}
+              onClick={handleLogin}
+              className="w-full h-11 rounded-xl 
+              bg-blue-600 hover:bg-blue-700 text-white 
+              flex items-center justify-center gap-2"
+            >
+              {loading && <Loader2 className="h-4 w-4 animate-spin" />}
+              {loading ? "Logging....." : "Login"}
+            </Button>
 
                     <div className="relative w-full">
                         <div className="absolute inset-0 flex items-center">
